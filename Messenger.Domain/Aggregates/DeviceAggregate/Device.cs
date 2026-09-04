8359ff0169cfe078f607c2bbf0986088b688b1b8
Ruslan_Uterly <kholikov.rus@mail.ru>
2026-09-04 15:04:51 +0300

    Добавил агрегат устройства пользователя

---
+++ Messenger.Domain/Aggregates/DeviceAggregate/Device.cs
@@ -0,0 +1,24 @@
+﻿using Messenger.Domain.Abstractions;
+using Messenger.Domain.Aggregates.UserAggregate;
+
+namespace Messenger.Domain.Aggregates.DeviceAggregate;
+
+public class Device : AggregateRoot
+{
+    public Guid UserId { get; private set; }
+
+    public string DeviceName { get; private set; }      // "Pixel 6", "iPhone 14"
+    public string DeviceType { get; private set; }      // "Android", "iOS", "Web"
+    public string DeviceId { get; private set; }        // Уникальный ID устройства
+
+    // E2EE Keys для этого устройства
+    public PublicKey IdentityKey { get; private set; }
+    public List<PublicKey> OneTimePreKeys { get; private set; }
+    public long SignedPreKeyId { get; private set; }
+
+    // Session
+    public string RefreshToken { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime LastActivityAt { get; private set; }
+    public bool IsActive { get; private set; }
+}
